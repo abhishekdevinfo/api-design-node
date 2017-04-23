8fd094343c061d37607ebce5dd6f70b3004a0d24
Abhishek Baliyan <abhi.info.dev@gmail.com>
2017-04-23 13:47:32 +0530

    Setup Server and Reading Index.html file by FS

--- server.js
+++ server.js
@@ -2,4 +2,24 @@
 // that will send back the index.html file on a GET request to '/'
 // it should then send back jsonData on a GET to /data
 
+var express = require('express');
+var fs = require('fs');
+
+var app = express();
+
 var jsonData = {count: 12, message: 'hey'};
+
+app.get('/', function(req, res) {
+    fs.readFile('../api-design-node/index.html', 'utf8', (err, data) => {
+        if (err) throw err;
+        res.send(data);
+    });
+});
+
+app.get('/data', function(req, res) {
+    res.send(jsonData);
+});
+
+app.listen(3000, function() {
+    console.log('Server running on 3000');
+});
